7a7a441a3200f03c2dd03c19f6e89c84567c85c3
Justin Babilino <jlbabilino@Justins-Air.verizon.net>
2022-01-28 14:22:01 -0500

    Started work on robot visuals

--- src/main/java/org/team2363/helixnavigator/Test.java
+++ src/main/java/org/team2363/helixnavigator/Test.java
@@ -3,21 +3,48 @@ package org.team2363.helixnavigator;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeLineCap;
 import javafx.stage.Stage;
 
 public class Test extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Circle paneCircle = new Circle(40);
-        Circle directCircle = new Circle(30);
-        Pane pane = new Pane(paneCircle);
-        StackPane outerStack = new StackPane(directCircle);
-        primaryStage.setScene(new Scene(outerStack));
-        // System.out.println(pane.getBoundsInParent().getWidth());
-        System.out.println(directCircle.getBoundsInParent().getWidth());
+        double robotLength = 50;
+        double robotWidth = 50;
+        double robotHeading = 30;
+        
+        Rectangle background = new Rectangle();
+        background.setFill(Color.LIGHTGRAY);
+        Rectangle robotRect = new Rectangle();
+        robotRect.setTranslateX(200);
+        robotRect.setTranslateY(200);
+        robotRect.setFill(null);
+        robotRect.setStrokeLineCap(StrokeLineCap.ROUND);
+        robotRect.setStrokeWidth(10);
+        robotRect.setStroke(Color.DEEPSKYBLUE);
+        robotRect.setArcWidth(6);
+        robotRect.setArcHeight(6);
+        robotRect.setWidth(robotLength);
+        robotRect.setHeight(robotWidth);
+        robotRect.setRotate(robotHeading);
+        
+        Pane pane = new Pane(background, robotRect);
+        pane.layoutBoundsProperty().addListener((obsVal, oldVal, newVal) -> {
+            background.setWidth(newVal.getWidth());
+            background.setHeight(newVal.getHeight());
+        });
+        primaryStage.setScene(new Scene(pane));
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(400);
+        robotRect.setOnMouseClicked(event -> {
+            System.out.println("Robot Rect");
+        });
+        background.setOnMouseClicked(event -> {
+            System.out.println("Background");
+        });
         primaryStage.show();
     }
 
