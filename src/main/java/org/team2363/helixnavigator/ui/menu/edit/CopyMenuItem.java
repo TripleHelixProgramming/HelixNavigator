911948a0e4ab4e5ca04ce3de74da46d752d1231a
Justin Babilino <jlbabilino@Justins-Air.verizon.net>
2022-04-06 19:57:08 -0400

    Added copy/paste

--- src/main/java/org/team2363/helixnavigator/ui/menu/edit/CopyMenuItem.java
+++ src/main/java/org/team2363/helixnavigator/ui/menu/edit/CopyMenuItem.java
@@ -1,9 +1,20 @@
 package org.team2363.helixnavigator.ui.menu.edit;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jlbabilino.json.JSONSerializer;
+
 import org.team2363.helixnavigator.document.DocumentManager;
+import org.team2363.helixnavigator.document.HPath;
+import org.team2363.helixnavigator.document.HPathElement;
+import org.team2363.helixnavigator.document.obstacle.HObstacle;
+import org.team2363.helixnavigator.document.waypoint.HWaypoint;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.KeyCombination;
 
 public class CopyMenuItem extends MenuItem {
@@ -18,6 +29,34 @@ public class CopyMenuItem extends MenuItem {
     }
 
     public void action(ActionEvent event) {
-        System.out.println("Copying");
+        String data = null;
+        if (documentManager.getIsDocumentOpen() && documentManager.getDocument().isPathSelected()) {
+            HPath path = documentManager.getDocument().getSelectedPath();
+            int selectedWaypointsCount = path.getWaypointsSelectionModel().getSelectedItems().size();
+            int selectedObstaclesCount = path.getObstaclesSelectionModel().getSelectedItems().size();
+            int totalCount = selectedWaypointsCount + selectedObstaclesCount;
+            if (totalCount == 0) {
+                data = JSONSerializer.serializeString(path);
+            } else if (totalCount == 1 && selectedWaypointsCount == 1) {
+                data = JSONSerializer.serializeString(path.getWaypointsSelectionModel().getSelectedItem());
+            } else if (totalCount == 1 && selectedObstaclesCount == 1) {
+                data = JSONSerializer.serializeString(path.getObstaclesSelectionModel().getSelectedItem());
+            } else {
+                List<HPathElement> list = new ArrayList<>();
+                for (HWaypoint waypoint : path.getWaypointsSelectionModel().getSelectedItems()) {
+                    list.add(waypoint);
+                }
+                for (HObstacle obstacle : path.getObstaclesSelectionModel().getSelectedItems()) {
+                    list.add(obstacle);
+                }
+                data = JSONSerializer.serializeString(list);
+            }
+        }
+        if (data != null) {
+            Clipboard systemClipboard = Clipboard.getSystemClipboard();
+            ClipboardContent content = new ClipboardContent();
+            content.putString(data);
+            systemClipboard.setContent(content);
+        }
     }
 }
--- src/main/java/org/team2363/helixnavigator/ui/menu/edit/PasteMenuItem.java
+++ src/main/java/org/team2363/helixnavigator/ui/menu/edit/PasteMenuItem.java
@@ -1,9 +1,22 @@
 package org.team2363.helixnavigator.ui.menu.edit;
 
+import com.jlbabilino.json.JSONArray;
+import com.jlbabilino.json.JSONDeserializer;
+import com.jlbabilino.json.JSONDeserializerException;
+import com.jlbabilino.json.JSONEntry;
+import com.jlbabilino.json.JSONObject;
+import com.jlbabilino.json.JSONParser;
+import com.jlbabilino.json.JSONParserException;
+
 import org.team2363.helixnavigator.document.DocumentManager;
+import org.team2363.helixnavigator.document.HDocument;
+import org.team2363.helixnavigator.document.HPath;
+import org.team2363.helixnavigator.document.obstacle.HObstacle;
+import org.team2363.helixnavigator.document.waypoint.HWaypoint;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.MenuItem;
+import javafx.scene.input.Clipboard;
 import javafx.scene.input.KeyCombination;
 
 public class PasteMenuItem extends MenuItem {
@@ -18,6 +31,49 @@ public class PasteMenuItem extends MenuItem {
     }
 
     public void action(ActionEvent event) {
-        System.out.println("Pasting");
+        Clipboard systemClipboard = Clipboard.getSystemClipboard();
+        if (systemClipboard.hasString()) {
+            String data = systemClipboard.getString();
+            try {
+                JSONEntry jsonEntry = JSONParser.parseJSONEntry(data);
+                paste(jsonEntry);
+            } catch (JSONParserException e) {
+            }
+        }
+    }
+
+    public void paste(JSONEntry jsonEntry) {
+        if (documentManager.getIsDocumentOpen()) {
+            HDocument document = documentManager.getDocument();
+            try {
+                if (jsonEntry.isObject()) {
+                    JSONObject jsonObject = (JSONObject) jsonEntry;
+                    if (jsonObject.containsKey("waypoints")) {
+                        HPath pastedPath = JSONDeserializer.deserialize(jsonEntry, HPath.class);
+                        document.getPaths().add(pastedPath);
+                        document.setSelectedPathIndex(document.getPaths().size() - 1);
+                    } else if (documentManager.getDocument().isPathSelected()) {
+                        if (jsonObject.containsKey("waypoint_type")) {
+                            HWaypoint waypoint = JSONDeserializer.deserialize(jsonEntry, HWaypoint.class);
+                            int index = document.getSelectedPath().getWaypointsSelectionModel().getSelectedIndex() + 1;
+                            document.getSelectedPath().getWaypoints().add(index, waypoint);
+                        } else if (jsonObject.containsKey("obstacle_type")) {
+                            HObstacle obstacle = JSONDeserializer.deserialize(jsonEntry, HObstacle.class);
+                            int index = document.getSelectedPath().getObstaclesSelectionModel().getSelectedIndex() + 1;
+                            document.getSelectedPath().getObstacles().add(index, obstacle);
+                        }
+                    }
+                } else if (jsonEntry.isArray()) {
+                    JSONArray jsonArray = (JSONArray) jsonEntry;
+                    System.out.println(jsonArray.toJSONText());
+                    for (JSONEntry entry : jsonArray) {
+                        // System.out.println(entry.toJSONText());
+                        // System.out.println();
+                        paste(entry);
+                    }
+                }
+            } catch (JSONDeserializerException e) {
+            }
+        }
     }
 }
