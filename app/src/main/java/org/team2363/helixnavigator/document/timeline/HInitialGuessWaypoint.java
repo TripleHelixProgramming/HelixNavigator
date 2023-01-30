381edf2aee2f0ab2aea978c626066e95c80aba8e
Justin Babilino <28580376+jlbabilino@users.noreply.github.com>
2023-01-30 13:32:03 -0500

    Initial Guess Waypoint heading handle (#5)
    * Add heading property to InitialGuessWaypoint
    
    * Visuals + dialog for initial guess heading

--- app/src/main/java/org/team2363/helixnavigator/document/timeline/HInitialGuessWaypoint.java
+++ app/src/main/java/org/team2363/helixnavigator/document/timeline/HInitialGuessWaypoint.java
@@ -3,11 +3,27 @@ package org.team2363.helixnavigator.document.timeline;
 import org.team2363.helixtrajectory.InitialGuessPoint;
 
 import com.jlbabilino.json.DeserializedJSONConstructor;
+import com.jlbabilino.json.DeserializedJSONObjectValue;
+import com.jlbabilino.json.DeserializedJSONTarget;
+import com.jlbabilino.json.SerializedJSONObjectValue;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.transform.Transform;
 
 public class HInitialGuessWaypoint extends HWaypoint {
+
+    private final DoubleProperty heading = new SimpleDoubleProperty(this, "heading", 0.0);
     
     @DeserializedJSONConstructor
     public HInitialGuessWaypoint() {
+    }
+
+    @Override
+    public void transformRelative(Transform transform) {
+        super.transformRelative(transform);
+        double deltaAngle = Math.atan2(transform.getMyx(), transform.getMxx());
+        setHeading(getHeading() + deltaAngle);
     }
 
     @Override
@@ -20,7 +36,19 @@ public class HInitialGuessWaypoint extends HWaypoint {
         return true;
     }
 
+    public final DoubleProperty headingProperty() {
+        return heading;
+    }
+    @DeserializedJSONTarget
+    public final void setHeading(@DeserializedJSONObjectValue(key = "heading") double value) {
+        heading.set(value);
+    }
+    @SerializedJSONObjectValue(key = "heading")
+    public final double getHeading() {
+        return heading.get();
+    }
+
     public InitialGuessPoint toInitialGuessPoint() {
-        return new InitialGuessPoint(getX(), getY(), 0.0);
+        return new InitialGuessPoint(getX(), getY(), getHeading());
     }
 }
--- app/src/main/java/org/team2363/helixnavigator/ui/editor/waypoint/InitialGuessWaypointView.java
+++ app/src/main/java/org/team2363/helixnavigator/ui/editor/waypoint/InitialGuessWaypointView.java
@@ -1,13 +1,85 @@
 package org.team2363.helixnavigator.ui.editor.waypoint;
 
 import org.team2363.helixnavigator.document.timeline.HInitialGuessWaypoint;
+import org.team2363.helixnavigator.global.Standards;
 
+import javafx.beans.property.DoubleProperty;
 import javafx.scene.paint.Color;
 
 public class InitialGuessWaypointView extends WaypointView {
+
+    private final RobotView robotView = new RobotView();
+
+    private final HInitialGuessWaypoint initialGuessWaypoint;
     
     public InitialGuessWaypointView(HInitialGuessWaypoint initialGuessWaypoint) {
         super(initialGuessWaypoint);
+
+        this.initialGuessWaypoint = initialGuessWaypoint;
+
         setWaypointFill(Color.SKYBLUE);
+
+        robotView.getView().getTransforms().add(centerTranslate);
+        robotView.headingProperty().bind(this.initialGuessWaypoint.headingProperty());
+        robotView.zoomScaleProperty().bind(zoomScaleProperty());
+        robotView.getView().setOnMouseDragged(event -> {
+            // System.out.println("Dot dragged");
+            double x = event.getX();
+            double y = event.getY();
+            double[] lockAngles = {-180, -90, 0, 90, 180};
+            double lockRadius = Standards.HEADING_LOCK_RADIUS;
+            double angle = Math.toDegrees(Math.atan2(y, x));
+            // System.out.println(angle);
+            for (double lockAngle : lockAngles) {
+                if (!event.isShiftDown() && Math.abs(angle - lockAngle) <= lockRadius) {
+                    if (lockAngle == -180) {
+                        lockAngle = 180;
+                    }
+                    angle = lockAngle;
+                    break;
+                }
+            }
+            initialGuessWaypoint.setHeading(angle * (-Math.PI/180));
+        });
+    }
+
+    public final DoubleProperty bumperLengthProperty() {
+        return robotView.bumperLengthProperty();
+    }
+
+    public final void setBumperLength(double value) {
+        robotView.setBumperLength(value);
+    }
+
+    public final double getBumperLength() {
+        return robotView.getBumperLength();
+    }
+
+    public final DoubleProperty bumperWidthProperty() {
+        return robotView.bumperWidthProperty();
+    }
+
+    public final void setBumperWidth(double value) {
+        robotView.setBumperWidth(value);
+    }
+
+    public final double getBumperWidth() {
+        return robotView.getBumperWidth();
+    }
+
+    public final DoubleProperty headingProperty() {
+        return robotView.headingProperty();
+    }
+
+    public final void setHeading(double value) {
+        robotView.setHeading(value);
+    }
+
+    public final double getHeading() {
+        return robotView.getHeading();
+    }
+
+    public RobotView getRobotView() {
+        return robotView;
     }
 }
